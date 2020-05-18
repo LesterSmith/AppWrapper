802dae52e6b24aaa7cb7cc8b84c9e97e99a36012
LesterSmith <46542486+LesterSmith@users.noreply.github.com>
2020-05-18 17:08:36 -0400

    Initial Commit

---
+++ AppWrapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace AppWrapper
+{
+    public class AppWrapper
+    {
+        public enum ReporterBreak
+        {
+            None = 0,
+            Day = 1,
+            Week = 2,
+            Month = 3
+        }
+
+        public enum WindowEventType
+        {
+            EventHook = 0, // SetWinEventHook to use events
+            Polling =1 // poll on timer thru GetWindowTitle
+        }
+        public static string DevTrkrConnectionString
+        {
+            get { return ConfigurationManager.ConnectionStrings[DBName].ConnectionString; }
+        }
+        //public static string devenv = "devenv";
+        //public const string VSCode = "Code";
+        //public const string VSCodeUnknown = "vscodeUnknown";
+        //public const string VSUnknown = "VSUnknown";
+        //public const string appUnknown = "appUnknown";
+        public const string AppName = "DevTracker";
+        public const string Settings = "Settings";
+        public const string MainOnTop = "MainOnTop";
+        //public const string RunFileWatcher = "RunFileWatcher";
+        public const string ProgramError = "Program Error";
+        public const string DBName = "DevTrkr";
+        // NOTE: Config Option Names
+        public const string PollingTimeInterval = "POLLINGTIMEINTERVAL";
+        public const string WindowTypeEvents = "WINDOWTYPEEVENTS";
+        public const string DevProjFileExtension = "IDEPROJECTFILEEXTENSION";
+        public const string RecordApps = "RECORDAPPS";
+        public const string RecordFiles = "RECORDFILES";
+        public const string CacheExpirationTime = "CACHEEXPIRATIONTIME";
+        public const string FileWatcherDirecory = "FILEWATCHERDIRECTORY";
+        public const string CalendarQueriedTime = "CALENDARQUERIEDTIME";
+        // NOTE: End of Config Option Names
+    }
+}
---
+++ Util.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppWrapper
+{
+    public static class Util
+    {
+        /// <summary>        /// Returns string from front of "name - nbr"
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static string GetStringFromFrontOfText(string text)
+        {
+            var ptr = text.IndexOf(" -");
+            return ptr < 0 ? string.Empty : text.Substring(0, ptr);
+        }
+
+        /// <summary>
+        /// Return (int)nbr from front of "nbr - string"
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static int GetNbrFromFrontOfText(string text)
+        {
+            var ptr = text.IndexOf(" -");
+            return int.Parse(text.Substring(0, ptr));
+        }
+
+        public static string GetStringFromEndOfText(string text)
+        {
+            var ptr = text.IndexOf("- ") + 2;
+            return text.Substring(ptr);
+        }
+
+        /// <summary>
+        /// Delimeter can only be one character, usually one space
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="delimiter"></param>
+        /// <returns></returns>
+        public static string GetStringFromEndOfText(string text, string delimiter)
+        {
+            var ptr = text.IndexOf(delimiter) + 1;
+            return text.Substring(ptr);
+        }
+
+        /// <summary>
+        /// Delimeter can only be one character, usually one space
+        /// </summary>
+        public static string GetStringFromStatOfText(string text, string delimiter)
+        {
+            return text.Substring(0, text.IndexOf(delimiter));
+        }
+
+    }
+}
